7c259721c8f5497d3b2d283ba72b52a49296fed0
Leandro Coutinho <lbcoutinho.blm@gmail.com>
2021-05-13 14:25:12 -0300

    Setup mysql connection

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -1,11 +1,15 @@
 package users
 
 import (
+	"github.com/lbcoutinho/bookstore_users-api/datasources/mysql/users_db"
 	"github.com/lbcoutinho/bookstore_users-api/utils/date_utils"
 	"github.com/lbcoutinho/bookstore_users-api/utils/errors"
 )
 
 func (u *User) Get() *errors.RestErr {
+	if err := users_db.Client.Ping(); err != nil {
+		panic(err)
+	}
 	return nil
 }
 
